188abae5c4bab5047fe3b2182ebab81a326b5fc9
Adrien Charrier <adrien.charrier@assurance-maladie.fr>
2025-12-19 11:57:35 +0100

    Update base path configuration in Vite for production environment

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: process.env.NODE_ENV === 'production' ? '/DatePicker/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
